a217689af931872d9c2b5e177be7694da31adbfd
Attila Blascsak <attila@blascsak.com>
2026-06-10 12:51:39 +0200

    Add Corsica as a dependency

--- conveyor_backend/mix.exs
+++ conveyor_backend/mix.exs
@@ -64,7 +64,8 @@ defmodule ConveyorBackend.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:corsica, "~> 2.1"}
     ]
   end
 
