9c3db71d37bf885a44d44ac63f47eb3137bfa76c
Attila Blascsak <attila@blascsak.com>
2026-06-10 12:18:45 +0200

    Fix issues reported by Credo

--- conveyor_backend/lib/conveyor_backend/accounts/user/senders/send_password_reset_email.ex
+++ conveyor_backend/lib/conveyor_backend/accounts/user/senders/send_password_reset_email.ex
@@ -13,8 +13,7 @@ defmodule ConveyorBackend.Accounts.User.Senders.SendPasswordResetEmail do
   @impl true
   def send(user, token, _) do
     new()
-    # TODO: Replace with your email
-    |> from({"noreply", "noreply@example.com"})
+    |> from({"noreply", "noreply@conveyor.com"})
     |> to(to_string(user.email))
     |> subject("Reset your password")
     |> html_body(body(token: token))
