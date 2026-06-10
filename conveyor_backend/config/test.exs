3073c1fe6378781d9fb0ef3d17310078dd0c9e97
Attila Blascsak <attila@blascsak.com>
2026-06-10 12:20:26 +0200

    Add a basic CI workflow

--- conveyor_backend/config/test.exs
+++ conveyor_backend/config/test.exs
@@ -13,10 +13,14 @@ config :conveyor_backend, ConveyorBackend.Mailer, adapter: Swoosh.Adapters.Test
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :conveyor_backend, ConveyorBackend.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "conveyor_backend_test#{System.get_env("MIX_TEST_PARTITION")}",
+  username: System.get_env("POSTGRES_USER", "postgres"),
+  password: System.get_env("POSTGRES_PASSWORD", "postgres"),
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
+  database:
+    System.get_env(
+      "POSTGRES_DB",
+      "conveyor_backend_test#{System.get_env("MIX_TEST_PARTITION")}"
+    ),
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
