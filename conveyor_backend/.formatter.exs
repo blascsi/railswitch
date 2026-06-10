f1a76a60f33ac9627d7576973dc173bf49aee370
Attila Blascsak <attila@blascsak.com>
2026-06-10 12:13:20 +0200

    Add Styler for formatting

--- conveyor_backend/.formatter.exs
+++ conveyor_backend/.formatter.exs
@@ -13,5 +13,5 @@
   ],
   subdirectories: ["priv/*/migrations"],
   inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}", "priv/*/seeds.exs"],
-  plugins: [Spark.Formatter]
+  plugins: [Spark.Formatter, Styler]
 ]
--- conveyor_backend/mix.exs
+++ conveyor_backend/mix.exs
@@ -55,6 +55,7 @@ defmodule ConveyorBackend.MixProject do
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},
       {:postgrex, ">= 0.0.0"},
+      {:styler, "~> 1.11", only: [:dev, :test], runtime: false},
       {:swoosh, "~> 1.16"},
       {:req, "~> 0.5"},
       {:telemetry_metrics, "~> 1.0"},
