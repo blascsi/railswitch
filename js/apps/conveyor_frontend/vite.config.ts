7153f178d93063f247e99e725c266a62ed4325d4
Attila Blascsak <attila@blascsak.com>
2026-06-27 16:28:13 +0200

    Update vite config with chunking

--- js/apps/conveyor_frontend/vite.config.ts
+++ js/apps/conveyor_frontend/vite.config.ts
@@ -5,4 +5,31 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  build: {
+    sourcemap: true,
+    cssCodeSplit: true,
+    chunkSizeWarningLimit: 600,
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          minSize: 20_000,
+          groups: [
+            {
+              name: "react-vendor",
+              test: /[\\/]node_modules[\\/](react|react-dom|scheduler)[\\/]/,
+              priority: 100,
+            },
+            {
+              name: "mantine",
+              test: /[\\/]node_modules[\\/]@mantine[\\/]/,
+              priority: 90,
+            },
+          ],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom", "wouter"],
+  },
 });
