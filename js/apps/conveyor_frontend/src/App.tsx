118bb22cac7be9f11d552a709ab64436cda9442a
Attila Blascsak <attila@blascsak.com>
2026-06-27 20:54:45 +0200

    Create simple authentication page component

--- js/apps/conveyor_frontend/src/App.tsx
+++ js/apps/conveyor_frontend/src/App.tsx
@@ -2,26 +2,34 @@ import "@mantine/core/styles.css";
 import "./App.css";
 
 import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Suspense } from "react";
 import { Route, Switch } from "wouter";
 import { PageErrorBoundary } from "./components/PageErrorBoundary";
 import { PageLoader } from "./components/PageLoader";
 import { lazyWithPreload } from "./utils/lazy-with-preload";
 
+const queryClient = new QueryClient();
+
 const AuthenticationPage = lazyWithPreload(
   () => import("./pages/Authentication.page"),
 );
 
+const HomePage = lazyWithPreload(() => import("./pages/Home.page"));
+
 export function App() {
   return (
-    <MantineProvider defaultColorScheme="auto">
-      <PageErrorBoundary>
-        <Suspense fallback={<PageLoader />}>
-          <Switch>
-            <Route path="/" component={AuthenticationPage} />
-          </Switch>
-        </Suspense>
-      </PageErrorBoundary>
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider defaultColorScheme="auto">
+        <PageErrorBoundary>
+          <Suspense fallback={<PageLoader />}>
+            <Switch>
+              <Route path="/" component={AuthenticationPage} />
+              <Route path="/home" component={HomePage} />
+            </Switch>
+          </Suspense>
+        </PageErrorBoundary>
+      </MantineProvider>
+    </QueryClientProvider>
   );
 }
--- js/apps/conveyor_frontend/src/hey-api.ts
+++ js/apps/conveyor_frontend/src/hey-api.ts
@@ -2,6 +2,7 @@ import type { CreateClientConfig } from "./generated/client/client.gen";
 
 export const createClientConfig: CreateClientConfig = (config) => ({
   ...config,
-  baseUrl: import.meta.env.VITE_API_URL,
+  baseUrl: import.meta.env.VITE_API_URL ?? "http://localhost:4000/api/json",
+  credentials: "include",
   throwOnError: true,
 });
