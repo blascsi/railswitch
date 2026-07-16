d895e90a16f35a32850b26f675db74e758fcaab9
Attila Blascsak <attila@blascsak.com>
2026-07-16 11:33:38 +0200

    Create schemas package with rules schema

---
+++ js/packages/schemas/src/index.ts
@@ -0,0 +1,11 @@
+export type {
+  AttributeCondition,
+  CombinatorOperator,
+  ComparisionOperator,
+  Condition,
+  ConditionGroup,
+  Conditions,
+  Rule,
+  Rules,
+} from "./rules.js";
+export { rulesSchema } from "./rules.js";
---
+++ js/packages/schemas/src/rules.ts
@@ -0,0 +1,82 @@
+import { z } from "zod";
+
+const comparisonOperatorSchema = z.enum([
+  "eq",
+  "neq",
+  "lt",
+  "lte",
+  "gt",
+  "gte",
+  "contains",
+  "not_contains",
+  "exists",
+  "not_exists",
+  "is_true",
+  "is_false",
+]);
+
+const combinatorOperatorSchema = z.enum(["and", "or"]);
+
+const attributeConditionSchema = z
+  .strictObject({
+    type: z.literal("attribute"),
+    attribute: z.string().min(1).trim(),
+    operator: comparisonOperatorSchema,
+    value: z.unknown().optional(),
+  })
+  .refine(
+    (val) => {
+      // Making sure that operations that can't accept values always pass
+      if (
+        ["exists", "not_exists", "is_true", "is_false"].includes(val.operator)
+      ) {
+        return true;
+      }
+
+      return val.value != null;
+    },
+    {
+      error: "Condition must have 'value' for comparison operators",
+    },
+  );
+
+const conditionSchema = z.discriminatedUnion("type", [
+  attributeConditionSchema,
+]);
+
+const conditionsSchema = z.array(
+  z.union([conditionSchema, z.lazy(() => conditionGroupSchema)]),
+);
+
+const conditionGroupSchema: z.ZodType<{
+  combinator: z.infer<typeof combinatorOperatorSchema>;
+  conditions: z.infer<typeof conditionsSchema>;
+}> = z.strictObject({
+  combinator: combinatorOperatorSchema,
+  conditions: conditionsSchema,
+});
+
+const valueResultSchema = z.strictObject({
+  type: z.literal("value"),
+  value: z.union([z.string(), z.number(), z.boolean()]),
+});
+
+const resultSchema = z.discriminatedUnion("type", [valueResultSchema]);
+
+const ruleSchema = z.strictObject({
+  description: z.string().optional(),
+  enabled: z.boolean(),
+  conditions: conditionGroupSchema,
+  result: valueResultSchema,
+});
+
+export const rulesSchema = z.array(ruleSchema);
+
+export type Rules = z.infer<typeof rulesSchema>;
+export type Rule = z.infer<typeof ruleSchema>;
+export type ConditionGroup = z.infer<typeof conditionGroupSchema>;
+export type Conditions = z.infer<typeof conditionsSchema>;
+export type Condition = z.infer<typeof conditionSchema>;
+export type AttributeCondition = z.infer<typeof attributeConditionSchema>;
+export type CombinatorOperator = z.infer<typeof combinatorOperatorSchema>;
+export type ComparisionOperator = z.infer<typeof comparisonOperatorSchema>;
